8a789685b293ba619ce63b7869714b71b767d1d0
Pramod Kumar <vpramodkumar@github.com>
2016-05-31 23:31:27 -0600

    feat: Implementation of random actions
    Implemented random actions for learning agent.

--- Project4-Smartcab/smartcab/smartcab/agent.py
+++ Project4-Smartcab/smartcab/smartcab/agent.py
@@ -25,7 +25,7 @@ class LearningAgent(Agent):
         # TODO: Update state
         
         # TODO: Select action according to your policy
-        action = None
+        action = random.choice(self.env.valid_actions)
 
         # Execute action and get reward
         reward = self.env.act(self, action)
