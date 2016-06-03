bff94e14f57bb31541606b0bf3e770265be064ce
Pramod Kumar <vpramodkumar@github.com>
2016-06-02 21:32:42 -0600

    feat: Implemented Q-Learning
    Added Q_Learning to train smart cab.

--- Project4-Smartcab/smartcab/smartcab/agent.py
+++ Project4-Smartcab/smartcab/smartcab/agent.py
@@ -10,11 +10,28 @@ class LearningAgent(Agent):
         super(LearningAgent, self).__init__(env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
+
         # TODO: Initialize any additional variables here
+        self.Q_Table = {}
+        self.valid_actions = self.env.valid_actions
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
         # TODO: Prepare for a new trip; reset any variables here, if required
+
+    #intialize all possible actions for a particular state in the Q-Table.
+    def init_Q_Values (self, state):
+        possible_actions = {possible_action: 0 for possible_action in self.valid_actions }
+        self.Q_Table[state] = possible_actions
+
+    #for the current state, iterates over all possible actions and returns the action which maximizes the Q value
+    def ArgMAX_Q (self, state):
+        return max(self.Q_Table[state].iteritems(), key= lambda x : x[1])[0]
+
+    #for the current state, iterates over all possible actions and returns the largest Q value
+    def MAX_Q (self, state):
+        return max(self.Q_Table[state].values())
+
 
     def update(self, t):
         # Gather inputs
@@ -24,16 +41,37 @@ class LearningAgent(Agent):
 
         # TODO: Update state
         #self.state = (inputs,self.next_waypoint,deadline)
-        self.state = [('time_left', deadline), ('light', inputs['light'] ), ('next_waypoint', self.next_waypoint)]
+        #this state will act as a key in the Q-table dictionary
+        self.state = (('time_left', deadline), ('light', inputs['light'] ), ('next_waypoint', self.next_waypoint))
 
-        
+        #initialize the state into the Q_Table will do nothing if it's already there.
+        if self.state not in self.Q_Table:
+            self.init_Q_Values(self.state)
+
         # TODO: Select action according to your policy
-        action = random.choice(self.env.valid_actions[1:])
+        #action = random.choice(self.env.valid_actions[1:])
+        action = self.ArgMAX_Q(self.state)
 
         # Execute action and get reward
         reward = self.env.act(self, action)
 
         # TODO: Learn policy based on state, action, reward
+        #sense environment again,  in order to update Q_Table.
+        inputs_next = self.env.sense(self)
+        deadline -= 1  #we have to force this because the deadline only updates with env.step(), it won't update from env.act().
+        self.next_waypoint = self.planner.next_waypoint()
+        state_next = (('time_left', deadline), ('light', inputs_next['light'] ), ('next_waypoint', self.next_waypoint))
+
+        #initialize the new state into the Q_Table will do nothing if it's already there.
+        if state_next not in self.Q_Table:
+            self.init_Q_Values(state_next)
+
+
+        # Q-learning Variables
+        alpha = .05
+        gamma = .5
+
+        self.Q_Table[self.state][action] = (1-alpha) * self.Q_Table[self.state][action] + alpha * (reward + gamma * self.MAX_Q(state_next))
 
         print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
 
@@ -44,12 +82,12 @@ def run():
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
     a = e.create_agent(LearningAgent)  # create agent
-    e.set_primary_agent(a, enforce_deadline=False)  # set agent to track
+    e.set_primary_agent(a, enforce_deadline=True)  # set agent to track
 
     # Now simulate it
-    sim = Simulator(e, update_delay=0.3)  # reduce update_delay to speed up simulation
-    sim.run(n_trials=10)  # press Esc or close pygame window to quit
+    sim = Simulator(e, update_delay=0.5)  # reduce update_delay to speed up simulation
+    sim.run(n_trials=100)  # press Esc or close pygame window to quit
 
-
+    
 if __name__ == '__main__':
     run()
